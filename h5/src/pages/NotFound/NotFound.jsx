6e64dd31388b8732f2a59a102aaaaaad7795db53
Chan Gavin <justin.coding@gmail.com>
2020-05-09 17:56:28 +0800

    h5-404页面

--- h5/src/pages/NotFound/NotFound.jsx
+++ h5/src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import notFoundIcon from "assets/404/404.jpg";
+
+import * as styles from "./notFound.module.scss";
 
 export default function NotFound() {
-  return <div>Not Found</div>;
+  const history = useHistory();
+
+  const goBack = () => {
+    history.push("/");
+  };
+
+  return (
+    <div className={styles["container"]}>
+      <img className={styles["icon"]} src={notFoundIcon} alt="404" />
+      <p className={styles["sorry"]}>抱歉，页面找不到了...</p>
+      <p className={styles["goto"]}>
+        <span onClick={goBack}>商城首页 mi.com</span>
+      </p>
+    </div>
+  );
 }
