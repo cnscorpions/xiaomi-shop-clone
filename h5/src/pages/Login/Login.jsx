60f9c5b6227e2431bc1e128ef4636233eb8f46fb
Chan Gavin <justin.coding@gmail.com>
2020-05-12 22:54:54 +0800

    h5-登录页面基础

--- h5/src/pages/Login/Login.jsx
+++ h5/src/pages/Login/Login.jsx
@@ -1,5 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Login() {
-  return <div>登录页</div>;
+import * as styles from "./login.module.scss";
+import miIcon from "assets/login/mi.png";
+import eyeIcon from "assets/login/eye.svg";
+import eyeHideIcon from "assets/login/eye-hide.svg";
+
+export default function Login(props) {
+  const [isPasswordHidden, setHide] = useState();
+  const [account, setAccount] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  // 切换显示
+  const shiftPwdVisibility = () => {
+    // 切换状态
+    let reverse = !isPasswordHidden;
+    setHide(reverse);
+    const inputEl = document.querySelector("input#password");
+    // 通过切换type值，实现隐藏pwd
+    if (isPasswordHidden) {
+      inputEl.setAttribute("type", "password");
+    } else {
+      inputEl.setAttribute("type", "text");
+    }
+  };
+
+  const handleClick = e => {
+    e.preventDefault();
+    console.log(account, pwd);
+  };
+
+  return (
+    <div className={styles["wrapper"]}>
+      <div className={styles["header-container"]}>
+        <img src={miIcon} />
+        <h4>小米帐号登录</h4>
+      </div>
+      <div>
+        <form className={styles["login-form"]}>
+          <input
+            id="account"
+            value={account}
+            onChange={e => setAccount(e.currentTarget.value)}
+            placeholder="邮箱/手机/小米ID"
+          />
+          <input
+            id="password"
+            value={pwd}
+            onChange={e => setPwd(e.currentTarget.value)}
+            placeholder="密码"
+          />
+          <button className={styles["login-form-btn"]} onClick={handleClick}>
+            登录
+          </button>
+          <img
+            className={styles["icon-eye"]}
+            src={isPasswordHidden ? eyeIcon : eyeHideIcon}
+            onClick={shiftPwdVisibility}
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
--- h5/src/pages/User/User.jsx
+++ h5/src/pages/User/User.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
+import userIcon from "assets/user/avatar.png";
+
+import * as styles from "./user.module.scss";
 
 export default function User() {
-  return <div>User</div>;
+  const history = useHistory();
+
+  const goLogin = () => {
+    alert("modal提示同意服务条款");
+    // history.push("/login");
+  };
+
+  return (
+    <div className={styles["wrapper"]}>
+      <div className={styles["login-box"]} onClick={goLogin}>
+        <img src={userIcon} />
+        <span>登录/注册</span>
+      </div>
+      <div className={styles["order-box"]}>
+        <span>我的订单</span>
+        <span>全部订单</span>
+      </div>
+      <ul></ul>
+    </div>
+  );
 }
