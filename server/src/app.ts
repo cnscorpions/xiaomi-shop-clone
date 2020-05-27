bed2e2588cf7ce8f83a7d0939f8a087f24c3c22c
Chan Gavin <justin.coding@gmail.com>
2020-05-28 00:03:43 +0800

    server-初步定下项目的架构

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express';
+import config from './config';
 
 const app: express.Application = express();
-const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello Express and Typescript');
 });
 
-app.listen(port, () => {
-    console.log(`server is listening on ${port}`);
+app.listen(config.port, () => {
+    console.log(`server is listening on ${config.port}`);
 });
